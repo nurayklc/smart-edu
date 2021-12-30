9c1af6d55522f911e5666bd7078c82b2309a6efa
nurayklc <nuriskl72@gmail.com>
2021-12-30 23:34:37 +0300

    Update links and pages.

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('INDEX SAYFASI');
+  res.status(200).render('index', {
+      pageName : "index"
+  });
 });
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    pageName : "about"
+});
+});
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
