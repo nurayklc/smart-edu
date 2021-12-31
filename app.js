07883a1b3813729b1020e72787c95b340b02afe6
nurayklc <nuriskl72@gmail.com>
2021-12-31 15:23:30 +0300

    Add express router

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const pageRouter = require('./routes/pageRouter')
 const app = express();
 
 // Template Engine
@@ -8,16 +8,7 @@ app.set('view engine', 'ejs');
 // Middlewares
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-      pageName : "index"
-  });
-});
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-    pageName : "about"
-});
-});
+app.use('/', pageRouter);
 
 const port = 3000;
 
