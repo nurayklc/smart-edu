94e9f49feeec82eab868088566f7b3d786daeda0
nurayklc <nuriskl72@gmail.com>
2021-12-31 22:29:17 +0300

    Connect DB.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const pageRouter = require('./routes/pageRouter')
+const courseRouter = require('./routes/courseRouter')
+
 const app = express();
+
+// Connect DB 
+mongoose.connect('mongodb://localhost/smartEdu');
 
 // Template Engine
 app.set('view engine', 'ejs');
@@ -9,9 +15,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.use('/', pageRouter);
+app.use('/course', courseRouter);
 
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
+  //console.log("Mutlu Yıllarrr Gençler :D")
 });
