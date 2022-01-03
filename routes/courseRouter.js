68721fc8918700d0de02c575eb9ee251bdefdf52
nurayklc <nuriskl72@gmail.com>
2022-01-04 00:53:31 +0300

    Add role middleware.

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/courseController');
+const roleMiddleware = require('../middlewares/roleMiddleware')
 
 router.route('/').get(courseController.getAllCourses)
-router.route('/').post(courseController.createCourse); //http://localhost:3000/course
+router.route('/').post(roleMiddleware(['teacher', 'admin']), courseController.createCourse); //http://localhost:3000/course
 router.route('/:slug').get(courseController.getCourse); 
 
 module.exports = router;
