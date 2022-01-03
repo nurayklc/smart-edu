5b778ba2bf23899b58f1760ed49454c719c736f2
nurayklc <nuriskl72@gmail.com>
2022-01-03 22:39:25 +0300

    Add logout, dashboard pages and session info.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const pageRouter = require('./routes/pageRouter');
 const courseRouter = require('./routes/courseRouter');
 const categoryRouter = require('./routes/categoryRoute');
@@ -13,11 +15,27 @@ mongoose.connect('mongodb://localhost/smartEdu');
 // Template Engine
 app.set('view engine', 'ejs');
 
+// Global Variables
+global.userIN = null;
+
 // Middlewares
+app.use(
+  session({
+    secret: 'smart_edu_keyboard',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartEdu' }),
+  })
+);
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // for parsing application/json
 
+// Routes
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use('/', pageRouter);
 app.use('/courses', courseRouter);
 app.use('/categories', categoryRouter);
--- controllers/authController.js
+++ controllers/authController.js
@@ -15,23 +15,38 @@ exports.createUser = async (req, res) => {
     });
   }
 };
+
 exports.loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
-
-    const user = await User.findOne({ email })
-      if (user) {
-        bcrypt.compare(password, user.password, (err, same) => {
-          if (same) {
-            // USER SESSION
-            res.status(200).send('YOU ARE LOGGED IN');
-          }
-        });
-      }
+    const user = await User.findOne({ email });
+    if (user) {
+      bcrypt.compare(password, user.password, (err, same) => {
+        if (same) {
+          // USER SESSION
+          req.session.userID = user._id;
+          res.status(200).redirect('/users/dashboard');
+        }
+      });
+    }
   } catch (error) {
     res.status(400).json({
       status: 'fail',
       error,
     });
   }
+};
+
+exports.logoutUser = (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
+};
+
+exports.getdashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+  res.status(200).render('dashboard', {
+    pageName: 'dashboard',
+    user,
+  });
 };
