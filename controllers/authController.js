149c1f7956e25e5c6d27287ab313a049892312fa
nurayklc <nuriskl72@gmail.com>
2022-01-09 23:30:00 +0300

    Add admin page

--- controllers/authController.js
+++ controllers/authController.js
@@ -56,10 +56,12 @@ exports.getdashboardPage = async (req, res) => {
   const user = await User.findOne({ _id: req.session.userID }).populate('courses');
   const categories = await Category.find()  
   const courses = await Course.find({user:req.session.userID})
+  const users = await User.find()
   res.status(200).render('dashboard', {
     pageName: 'dashboard',
     user,
     categories,
-    courses
+    courses,
+    users
   });
 };
