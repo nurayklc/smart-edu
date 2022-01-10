c1c423b04eb7a8c1c0a43d4eae3bd7364afb342e
nurayklc <nuriskl72@gmail.com>
2022-01-10 17:38:07 +0300

    make it better

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use('/courses', courseRouter);
 app.use('/categories', categoryRouter);
 app.use('/users', userRouter);
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
