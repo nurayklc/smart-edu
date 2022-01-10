0a19dbfc844335be74af7febb3258a283e1dfebf
nurayklc <nuriskl72@gmail.com>
2022-01-10 15:14:07 +0300

    Add category add,update,delete.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,14 +3,34 @@ const Category = require('../models/Category');
 exports.createCategory = async (req, res) => {
   try {
     const category = await Category.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      category,
-    });
+    req.flash('success', `${category.name} has been remove successfuly`);
+    res.status(200).redirect('/users/dashboard');
   } catch (error) {
-    res.status(400).json({
-      status: 'failed',
-      error,
-    });
+    req.flash('error', `Something happened!`);
+    res.status(400).redirect('/users/dashboard');
+  }
+};
+
+exports.deleteCategory = async (req, res) => {
+  try {
+    const category = await Category.findByIdAndRemove(req.params.id)
+    req.flash('success', `${category.name} has been remove successfuly`);
+    res.status(200).redirect('/users/dashboard');
+  } catch (error) {
+    req.flash('error', `Something happened!`);
+    res.status(400).redirect('/users/dashboard');
+  }
+};
+
+exports.updateCategory = async (req, res) => {
+  try {
+    const category = await Category.findOne({id: req.params.id})
+    category.name = req.body.name
+    category.save()
+    req.flash('success', `${category.name} has been update successfuly`);
+    res.status(200).redirect('/users/dashboard');
+  } catch (error) {
+    req.flash('error', `Something happened!`);
+    res.status(400).redirect('/users/dashboard');
   }
 };
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 
 router.route('/').post(categoryController.createCategory); //http://localhost:3000/categories
+router.route('/:id').delete(categoryController.deleteCategory);
+router.route('/:id').put(categoryController.updateCategory);
 
 module.exports = router;
